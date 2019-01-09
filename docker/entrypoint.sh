2228b53abcfc7d3c8c1d551b5093202f86b6cbeb
kamino <lishichenjs@163.com>
2019-01-09 13:27:45 +0800

    pyweb

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/conda/bin/uwsgi --chdir /app/www/isdut.cn --wsgi-file isdut/wsgi.py --socket 172.18.5.5:9090
+/opt/conda/bin/uwsgi --chdir /app/www/isdut.cn/isdut --wsgi-file isdut/wsgi.py --socket 172.18.5.5:9090
